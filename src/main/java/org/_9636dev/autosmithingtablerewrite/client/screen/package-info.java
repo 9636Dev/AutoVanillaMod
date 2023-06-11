ddbbe5f565a122b03f451fb83c30d722956fe1e6
HW9636 <9636dev@gmail.com>
2023-06-11 11:47:40 +0200

    Imported 1.20 forge MDK

--- src/main/java/org/_9636dev/autosmithingtablerewrite/client/screen/package-info.java
+++
@@ -1,5 +0,0 @@
-@OnlyIn(Dist.CLIENT)
-package org._9636dev.autosmithingtablerewrite.client.screen;
-
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
--- src/main/java/org/_9636dev/autosmithingtablerewrite/common/recipe/AutoSmithingRecipeBuilder.java
+++
@@ -1,6 +0,0 @@
-package org._9636dev.autosmithingtablerewrite.common.recipe;
-
-@SuppressWarnings("unused")
-public class AutoSmithingRecipeBuilder {
-    // TODO: 28.05.23 Implement
-}
